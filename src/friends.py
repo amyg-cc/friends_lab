6284e3b19a1665e000c19098b4467cc3e94e2433
amy gallacher <amy.gallacher@codeclan.com>
2021-08-05 11:24:06 +0100

    passes test 3

--- src/friends.py
+++ src/friends.py
@@ -3,3 +3,10 @@ def get_name(person):
 
 def get_favourite_tv_show(person):
     return person["favourites"]["tv_show"]
+
+def likes_to_eat(person, food):
+    snack_match = False
+    for snack in person["favourites"]["snacks"]:
+        if food in person["favourites"]["snacks"]:
+            snack_match = True
+    return snack_match
